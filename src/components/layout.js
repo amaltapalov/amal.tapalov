b46553e274163f192419ff37fae1c4f2e40cdbf3
amal <amal.t@tabella.cz>
2019-06-05 16:13:44 +0200

    decrease spacing for Title

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,8 +13,8 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            ...scale(1),
+            marginBottom: rhythm(1),
             marginTop: 0,
           }}
         >
@@ -62,11 +62,7 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <footer>© {new Date().getFullYear()} Amal.Tapalov</footer>
       </div>
     )
   }
